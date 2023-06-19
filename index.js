bc4d91063969f59656d87642e5f96d393632b7a2
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-19 14:10:15 -0400

    wriet test code for fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
-
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
 // fetchCoordsByIP('192.0.131.131',(error, data) => {
@@ -17,3 +16,11 @@ fetchMyIP((error, ip) => {
 //   }
 //   console.log(data);
 // });
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+fetchISSFlyOverTimes(exampleCoords), (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:' , passTimes);
+};
