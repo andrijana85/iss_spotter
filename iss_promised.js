6325dc5e3f4f24aa4e0aaf6bff705ce0bdf69174
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-19 22:05:52 -0400

    modify fetchISSFlyOverTimes

--- iss_promised.js
+++ iss_promised.js
@@ -1,7 +1,7 @@
 const request = require('request-promise-native');
 
 //Requests user's ip address from https://www.ipify.org/
-const fetchMyIP = function () {
+const fetchMyIP = function() {
 
   //Returns: Promise of request for ip data, returned as JSON string
   return request('https://api.ipify.org?format=json');
@@ -12,5 +12,10 @@ const fetchCoordsByIP = function(body) {
   //Make a request to ipwho.is and return the promise from request
   return request(`http://ipwho.is/${ip}`);
 };
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
